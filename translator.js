0f597a49e3d1f16338a930eb976b096605784ec1
Airam <82757584+Airam9598@users.noreply.github.com>
2021-08-31 19:34:13 +0100

    New animations

--- translator.js
+++ translator.js
@@ -48,3 +48,30 @@
 
         // layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
     }
+
+    document.getElementById("p1").onmouseover = function() {mouseOver("p1")};
+    document.getElementById("p2").onmouseover = function() {mouseOver("p2")};
+    document.getElementById("p3").onmouseover = function() {mouseOver("p3")};
+    document.getElementById("p4").onmouseover = function() {mouseOver("p4")};
+    document.getElementById("p5").onmouseover = function() {mouseOver("p5")};
+
+    document.getElementById("p1").onmouseout = function() {mouseOut()};
+    document.getElementById("p2").onmouseout = function() {mouseOut()};
+    document.getElementById("p3").onmouseout = function() {mouseOut()};
+    document.getElementById("p4").onmouseout = function() {mouseOut()};
+    document.getElementById("p5").onmouseout = function() {mouseOut()};
+    function mouseOver(data) {
+        for(var i=1;i<6;i++){
+            document.getElementById("p"+i).style.opacity = "0.5";
+            document.getElementById("p"+i).style.brightness = "0.5";
+        }
+        document.getElementById(data).style.opacity = "1";
+        document.getElementById(data).style.brightness = "1";
+    }
+
+    function mouseOut() {
+        for(var i=1;i<6;i++){
+            document.getElementById("p"+i).style.opacity = "1";
+            document.getElementById("p"+i).style.brightness = "1";
+        }
+    }
